0a3c0bdfee8edde7cd2f257d0e1d39dce9611993
Aleks <aleks.07k.ua@gmail.com>
2026-07-11 20:20:47 +0300

    Add goals to analysis context

--- backend/core/analysis_context.py
+++ backend/core/analysis_context.py
@@ -5,11 +5,19 @@ Analysis context builder
 
 
 class AnalysisContext:
-    def __init__(self, question: str, user_context=None, knowledge_context=None, memory_context=None):
+    def __init__(
+        self,
+        question: str,
+        user_context=None,
+        knowledge_context=None,
+        memory_context=None,
+        goals: list = None,
+    ):
         self.question = question
         self.user_context = user_context
         self.knowledge_context = knowledge_context
         self.memory_context = memory_context
+        self.goals = goals or []
 
     def to_dict(self) -> dict:
         return {
@@ -17,4 +25,5 @@ class AnalysisContext:
             "user_context": self.user_context,
             "knowledge_context": self.knowledge_context,
             "memory_context": self.memory_context,
+            "goals": self.goals,
         }
