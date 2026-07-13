4b0658e2b91eb2849b22160ef0da9df454c00b32
Aleks <aleks.07k.ua@gmail.com>
2026-07-13 20:56:21 +0300

    Wire AnalysisEngine into StrategicExecutor flow

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ from engines.input_analysis_engine import InputAnalysisEngine
 from engines.skill_execution_engine import SkillExecutionEngine
 from engines.methodology_planner import MethodologyPlanner
 from engines.reasoning_builder import ReasoningBuilder
+from engines.reasoning_analysis_engine import AnalysisEngine
 from preferences.preferences_manager import PreferencesManager
 from agents.strategic_analyst import StrategicAnalyst
 from agents.economic_analyst import EconomicAnalyst
@@ -66,6 +67,7 @@ def main():
     information_manager = InformationManager()
     strategic_executor = StrategicExecutor(
         reasoning_pipeline=ReasoningPipeline(SkillExecutionEngine(), MethodologyPlanner(), ReasoningBuilder()),
+        analysis_engine=AnalysisEngine(),
         user_context=memory_context,
         information_manager=information_manager,
     )
